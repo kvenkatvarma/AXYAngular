4b5abb8ab8a63f0afc445fc8131186cccbcfa247
Venkat varma <kvenkatvarma@gmail.com>
2025-02-26 17:47:29 +0530

    Routing and Adding Toaster notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,9 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     FormsModule,
+    ToastrModule.forRoot({
+      positionClass : 'toast-bottom-right'
+    }),
     BsDropdownModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
